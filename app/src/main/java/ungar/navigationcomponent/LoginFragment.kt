adbf1159ca66bac856bf027eb59a7910af7bb722
Borna Ungar <borna.ungar@gmail.com>
2020-12-26 22:47:12 +0100

    deep links (implicit)

--- app/src/main/java/ungar/navigationcomponent/LoginFragment.kt
+++ app/src/main/java/ungar/navigationcomponent/LoginFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private val args: LoginFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //implicit deep links, in this example going to web and typing
+        //'example.com/links/{some_username}, the phone will ask you
+        //which app to use, and using this one it will open the login
+        //fragment with username filled with the one you put into the link.
+        val usernameDeepLink = args.username
+        edit_text_username.setText(usernameDeepLink)
 
         btnConfirm.setOnClickListener {
             //https://youtu.be/i3nRZEbkWks?t=367
